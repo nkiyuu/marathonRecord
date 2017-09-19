2aacf46ecb52933bd5ee7e0bf9e9f04aedf091ed
Yuki Nozaki <yuuki060127@gmail.com>
2017-09-19 10:59:04 +0900

    orm使って扱えるようにしてみた

--- backend-app/models/model_parent.py
+++ backend-app/models/model_parent.py
@@ -1,32 +1,33 @@
-import re
-import datetime as dt
 import sqlite3
+import sqlalchemy
+import sqlalchemy.sql
+import sqlalchemy.orm
+import sqlalchemy.ext.declarative
+from sqlalchemy.dialects.sqlite import DATE
+import datetime
 
 from config import DB_PATH
 
+Base = sqlalchemy.ext.declarative.declarative_base()
+url = 'sqlite:///' + DB_PATH
+engine = sqlalchemy.create_engine(url, echo=True)
+Session = sqlalchemy.orm.sessionmaker(bind=engine)
 
-class Parent():
-    def __init__(self):
-        '''
-        初期設定をしてdbに接続する
-        '''
-        self.closed = False
-        self.db_path = DB_PATH
 
-        self.connection = sqlite3.connect(
-            self.db_path,
-            detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
-        )
-        self.cursor = self.connection.cursor()
+class UserModel(Base):
+    '''
+    usersテーブルのカラム
+    '''
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    name = sqlalchemy.Column(sqlalchemy.Text, unique=True, nullable=False)
+    created_at = sqlalchemy.Column(sqlalchemy.Date, server_default=sqlalchemy.sql.func.now(), nullable=False)
+    delete_at = sqlalchemy.Column(sqlalchemy.Text, default=None)
 
-    def __del__(self):
-        return self.close()
 
-    def __enter__(self):
-        return self
-
-    def close(self):
-        if self.closed:
-            return
-        self.closed = True
-        self.connection.close()
+def create_tables():
+    '''
+    テーブルデータの作成
+    :return:
+    '''
+    Base.metadata.create_all(engine)
--- backend-app/models/users.py
+++ backend-app/models/users.py
@@ -1,42 +1,13 @@
-from models.model_parent import Parent
+from models.model_parent import *
 
 
-class UserModel(Parent):
-
-    def __init__(self):
-        super().__init__()
-
-    def get_all(self):
-        '''
-        データベース上のすべてのユーザを取得
-        :return: データベース上のすべてのユーザ
-                 [{id: 1, name: hogehoge}, {id: 2, name: fugafuga}, ...]
-        '''
-        users_map = self.cursor.execute('select id, name from users')
-        users = []
-        for user_data in users_map:
-            users.append({"id": user_data[0], "name": user_data[1]})
-        return users
-
-    def get_user_by_id(self, id):
-        '''
-        idで指定したユーザを取得
-        :param id: user_id
-        :return: idで指定したユーザ
-                 {id: 3, name: hoge}
-        '''
-        user_map = self.cursor.execute('select id, name from users where id == ?', (id,)).fetchone()
-        if user_map is None:
-            return {}
-        return {"id": user_map[0], "name": user_map[1]}
-
-    def post_user(self, name):
-        '''
-        ユーザ1人を登録
-        :param name: ユーザネーム
-        :return: 追加したユーザ
-                 {id: 4, name: hoge}
-        '''
-        self.cursor.execute('insert into users (name) values ( ? )', (name,))
-        self.connection.commit()
-        return {"id": self.cursor.lastrowid, "name": name }
+def get_records_all():
+    '''
+    記録全件を取得
+    :return:  データベース上のすべての記録
+    '''
+    session = Session()
+    users = session.query(UserModel).all()
+    print(users[0].name)
+    session.flush()
+    session.close()
